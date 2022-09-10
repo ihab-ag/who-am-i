05487a5457af8aca69fb1cf29f45c16a36076a09
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-10 14:32:07 +0300

    function to get gender from api

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ window.onload = (event) =>{
     const submitBtn= document.getElementById('submitBtn');
     // variables
     let name = '';
+    let gender='';
 
     //events
     submitBtn.onclick=()=>{
         getName();
+        getGender();
     }
 
     //functions
     function getName(){
         name = input.value;
+    }
+    async function getGender(name) {
+        let api = 'https://api.genderize.io/?name=';
+        api = api.concat(name);
+        let x = await fetch(api);
+        let y = await x.json();
+        gender = y["gender"];
     }
 }
