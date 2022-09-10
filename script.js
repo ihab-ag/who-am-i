48fff1806e9d28aca753b4e74537fb6b0b2a522c
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-10 13:58:39 +0300

    added get name function

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ window.onload = (event) =>{
     // elements
     const input= document.getElementById('input');
     const submitBtn= document.getElementById('submitBtn');
+    // variables
+    let name = '';
+
+    //implementation
+    
+
+    //functions
+    function getName(){
+        name = input.value;
+    }
 }
